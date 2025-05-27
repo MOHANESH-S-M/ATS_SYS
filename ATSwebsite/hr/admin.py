ae92aa52a653f03b39104abb9e806d60cf5d12bf
MOHANESH <smmohanesh3@gmail.com>
2025-05-27 10:24:13 +0530

    models done hr

--- ATSwebsite/hr/admin.py
+++ ATSwebsite/hr/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import job_post
 # Register your models here.
+admin.site.register(job_post)
--- ATSwebsite/hr/models.py
+++ ATSwebsite/hr/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class job_post(models.Model):
+    title = models.CharField(max_length=70)
+    decscription = models.TextField()
+    location = models.CharField(max_length=70)
+    deadline = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
